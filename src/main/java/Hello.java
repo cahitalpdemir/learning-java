996a6853ec138b04ee2e7f3b4a691d064706ae58
cale <cahitalpdemir@gmail.com>
2025-02-06 20:46:18 +0100

    v1

---
+++ src/main/java/Hello.java
@@ -0,0 +1,5 @@
+public class Hello {
+     String string ="This is a string";
+
+    String string1 ="Git init is used only one per repository";
+}
---
+++ src/main/java/World.java
@@ -0,0 +1,3 @@
+public class World {
+    Integer integer =1; // non primitive types do not have a default value
+}
