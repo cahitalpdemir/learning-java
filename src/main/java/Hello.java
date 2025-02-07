4b5fe8c2160bdab81b707d1b61a78ba4ed6b3642
cahitalpdemir <cahitalpdemir@gmail.com>
2025-02-07 20:26:07 +0100

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -2,4 +2,5 @@ public class Hello {
      String string ="This is a string";
 
     String string1 ="Git init is used only one per repository";
+     String string2= "Developer B is add something"
 }
